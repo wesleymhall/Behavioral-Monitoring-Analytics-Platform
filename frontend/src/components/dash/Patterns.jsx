7784ff775e00afca3298a50c721a9acb09e9b8d3
wesleymhall <wesleyhall2002@gmail.com>
2025-08-28 12:58:31 -0400

    configured patterns frontend

--- frontend/src/components/dash/Patterns.jsx
+++ frontend/src/components/dash/Patterns.jsx
@@ -1,20 +1,40 @@
+import { metricConfig } from '../../Metrics.js';
 import { useState, useEffect } from 'react';
 
 function Patterns({ analytics }) {
-    const [patterns, setCurrPatterns] = useState({});
-    
+    const [clusters, setCurrClusters] = useState([]);
+    const [prediction, setCurrPrediction] = useState({});
+
     useEffect(() => {
         const getCurrPatterns = () => {
-            const patterns = analytics?.['patterns'];
-            setCurrPatterns(patterns);
+            const clusters = analytics?.['patterns']['clusters'] || [];
+            console.log(clusters);
+            const prediction = analytics?.['patterns']['prediction'] || {};
+            setCurrClusters(clusters);
+            setCurrPrediction(prediction);
         }
         getCurrPatterns();
-    }, [ analytics ]);
+    }, [analytics]);
 
     return (
         <div className='vertical-flex'>
             <p className='horizontal-left'>patterns</p>
-            <div>{JSON.stringify(patterns)}</div>
+            {clusters.length > 0 && prediction && (
+                <div className='vertical-flex'>
+                    {clusters.map((cluster, idx) => (
+                        <div className='horizontal-flex' key={idx}>
+                            <div className='table-value'>{idx + 1}</div>
+                            {Object.values(metricConfig).map(metric => {
+                                const val = cluster[metric.name];
+                                return (
+                                    <div className='table-label' key={metric.name}>{metric.emoji}: {val != null ? val : '-'}</div>
+                                )
+                            })}
+                            <div className='table-label'>%: {cluster['%']}</div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
